6f631649dff3618da7e0ee1961a0fa304a639842
AyeshaAreej <ayeshaareej222@gmail.com>
2022-08-17 22:43:54 +0500

    Admin Dashboard updated

--- frontend/src/dashboard/Dashboard.js
+++ frontend/src/dashboard/Dashboard.js
@@ -32,7 +32,7 @@ export default class Dashboard extends Component {
             <div className="small-box bg-info">
               <div className="inner">
                 <h3>150</h3>
-                <p>New Orders</p>
+                <p>New Requests</p>
               </div>
               <div className="icon">
                 <i className="ion ion-bag" />
@@ -45,8 +45,8 @@ export default class Dashboard extends Component {
             {/* small box */}
             <div className="small-box bg-success">
               <div className="inner">
-                <h3>53<sup style={{fontSize: 20}}>%</sup></h3>
-                <p>Bounce Rate</p>
+                <h3>53</h3>
+                <p>Registered Companies</p>
               </div>
               <div className="icon">
                 <i className="ion ion-stats-bars" />
@@ -54,27 +54,14 @@ export default class Dashboard extends Component {
               <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right" /></a>
             </div>
           </div>
+         
           {/* ./col */}
           <div className="col-lg-3 col-6">
             {/* small box */}
             <div className="small-box bg-warning">
               <div className="inner">
-                <h3>44</h3>
-                <p>User Registrations</p>
-              </div>
-              <div className="icon">
-                <i className="ion ion-person-add" />
-              </div>
-              <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right" /></a>
-            </div>
-          </div>
-          {/* ./col */}
-          <div className="col-lg-3 col-6">
-            {/* small box */}
-            <div className="small-box bg-danger">
-              <div className="inner">
                 <h3>65</h3>
-                <p>Unique Visitors</p>
+                <p>Registered Vendors</p>
               </div>
               <div className="icon">
                 <i className="ion ion-pie-graph" />
@@ -83,7 +70,22 @@ export default class Dashboard extends Component {
             </div>
           </div>
           {/* ./col */}
+           {/* ./col */}
+           <div className="col-lg-3 col-6">
+            {/* small box */}
+            <div className="small-box bg-danger">
+              <div className="inner">
+                <h3>1000</h3>
+                <p>User Registrations</p>
+              </div>
+              <div className="icon">
+                <i className="ion ion-person-add" />
+              </div>
+              <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right" /></a>
+            </div>
+          </div>
         </div>
+        
         {/* /.row */}
         {/* Main row */}
         <div className="row">
@@ -120,200 +122,14 @@ export default class Dashboard extends Component {
               </div>{/* /.card-body */}
             </div>
             {/* /.card */}
-            {/* DIRECT CHAT */}
-            <div className="card direct-chat direct-chat-primary">
-              <div className="card-header">
-                <h3 className="card-title">Direct Chat</h3>
-                <div className="card-tools">
-                  <span data-toggle="tooltip" title="3 New Messages" className="badge badge-primary">3</span>
-                  <button type="button" className="btn btn-tool" data-card-widget="collapse">
-                    <i className="fas fa-minus" />
-                  </button>
-                  <button type="button" className="btn btn-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
-                    <i className="fas fa-comments" />
-                  </button>
-                  <button type="button" className="btn btn-tool" data-card-widget="remove"><i className="fas fa-times" />
-                  </button>
-                </div>
-              </div>
-              {/* /.card-header */}
-              <div className="card-body">
-                {/* Conversations are loaded here */}
-                <div className="direct-chat-messages">
-                  {/* Message. Default to the left */}
-                  <div className="direct-chat-msg">
-                    <div className="direct-chat-infos clearfix">
-                      <span className="direct-chat-name float-left">Alexander Pierce</span>
-                      <span className="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
-                    </div>
-                    {/* /.direct-chat-infos */}
-                    <img className="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image" />
-                    {/* /.direct-chat-img */}
-                    <div className="direct-chat-text">
-                      Is this template really for free? That's unbelievable!
-                    </div>
-                    {/* /.direct-chat-text */}
-                  </div>
-                  {/* /.direct-chat-msg */}
-                  {/* Message to the right */}
-                  <div className="direct-chat-msg right">
-                    <div className="direct-chat-infos clearfix">
-                      <span className="direct-chat-name float-right">Sarah Bullock</span>
-                      <span className="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
-                    </div>
-                    {/* /.direct-chat-infos */}
-                    <img className="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image" />
-                    {/* /.direct-chat-img */}
-                    <div className="direct-chat-text">
-                      You better believe it!
-                    </div>
-                    {/* /.direct-chat-text */}
-                  </div>
-                  {/* /.direct-chat-msg */}
-                  {/* Message. Default to the left */}
-                  <div className="direct-chat-msg">
-                    <div className="direct-chat-infos clearfix">
-                      <span className="direct-chat-name float-left">Alexander Pierce</span>
-                      <span className="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
-                    </div>
-                    {/* /.direct-chat-infos */}
-                    <img className="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image" />
-                    {/* /.direct-chat-img */}
-                    <div className="direct-chat-text">
-                      Working with AdminLTE on a great new app! Wanna join?
-                    </div>
-                    {/* /.direct-chat-text */}
-                  </div>
-                  {/* /.direct-chat-msg */}
-                  {/* Message to the right */}
-                  <div className="direct-chat-msg right">
-                    <div className="direct-chat-infos clearfix">
-                      <span className="direct-chat-name float-right">Sarah Bullock</span>
-                      <span className="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
-                    </div>
-                    {/* /.direct-chat-infos */}
-                    <img className="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image" />
-                    {/* /.direct-chat-img */}
-                    <div className="direct-chat-text">
-                      I would love to.
-                    </div>
-                    {/* /.direct-chat-text */}
-                  </div>
-                  {/* /.direct-chat-msg */}
-                </div>
-                {/*/.direct-chat-messages*/}
-                {/* Contacts are loaded here */}
-                <div className="direct-chat-contacts">
-                  <ul className="contacts-list">
-                    <li>
-                      <a href="#">
-                        <img className="contacts-list-img" src="dist/img/user1-128x128.jpg" />
-                        <div className="contacts-list-info">
-                          <span className="contacts-list-name">
-                            Count Dracula
-                            <small className="contacts-list-date float-right">2/28/2015</small>
-                          </span>
-                          <span className="contacts-list-msg">How have you been? I was...</span>
-                        </div>
-                        {/* /.contacts-list-info */}
-                      </a>
-                    </li>
-                    {/* End Contact Item */}
-                    <li>
-                      <a href="#">
-                        <img className="contacts-list-img" src="dist/img/user7-128x128.jpg" />
-                        <div className="contacts-list-info">
-                          <span className="contacts-list-name">
-                            Sarah Doe
-                            <small className="contacts-list-date float-right">2/23/2015</small>
-                          </span>
-                          <span className="contacts-list-msg">I will be waiting for...</span>
-                        </div>
-                        {/* /.contacts-list-info */}
-                      </a>
-                    </li>
-                    {/* End Contact Item */}
-                    <li>
-                      <a href="#">
-                        <img className="contacts-list-img" src="dist/img/user3-128x128.jpg" />
-                        <div className="contacts-list-info">
-                          <span className="contacts-list-name">
-                            Nadia Jolie
-                            <small className="contacts-list-date float-right">2/20/2015</small>
-                          </span>
-                          <span className="contacts-list-msg">I'll call you back at...</span>
-                        </div>
-                        {/* /.contacts-list-info */}
-                      </a>
-                    </li>
-                    {/* End Contact Item */}
-                    <li>
-                      <a href="#">
-                        <img className="contacts-list-img" src="dist/img/user5-128x128.jpg" />
-                        <div className="contacts-list-info">
-                          <span className="contacts-list-name">
-                            Nora S. Vans
-                            <small className="contacts-list-date float-right">2/10/2015</small>
-                          </span>
-                          <span className="contacts-list-msg">Where is your new...</span>
-                        </div>
-                        {/* /.contacts-list-info */}
-                      </a>
-                    </li>
-                    {/* End Contact Item */}
-                    <li>
-                      <a href="#">
-                        <img className="contacts-list-img" src="dist/img/user6-128x128.jpg" />
-                        <div className="contacts-list-info">
-                          <span className="contacts-list-name">
-                            John K.
-                            <small className="contacts-list-date float-right">1/27/2015</small>
-                          </span>
-                          <span className="contacts-list-msg">Can I take a look at...</span>
-                        </div>
-                        {/* /.contacts-list-info */}
-                      </a>
-                    </li>
-                    {/* End Contact Item */}
-                    <li>
-                      <a href="#">
-                        <img className="contacts-list-img" src="dist/img/user8-128x128.jpg" />
-                        <div className="contacts-list-info">
-                          <span className="contacts-list-name">
-                            Kenneth M.
-                            <small className="contacts-list-date float-right">1/4/2015</small>
-                          </span>
-                          <span className="contacts-list-msg">Never mind I found...</span>
-                        </div>
-                        {/* /.contacts-list-info */}
-                      </a>
-                    </li>
-                    {/* End Contact Item */}
-                  </ul>
-                  {/* /.contacts-list */}
-                </div>
-                {/* /.direct-chat-pane */}
-              </div>
-              {/* /.card-body */}
-              <div className="card-footer">
-                <form action="#" method="post">
-                  <div className="input-group">
-                    <input type="text" name="message" placeholder="Type Message ..." className="form-control" />
-                    <span className="input-group-append">
-                      <button type="button" className="btn btn-primary">Send</button>
-                    </span>
-                  </div>
-                </form>
-              </div>
-              {/* /.card-footer*/}
-            </div>
+           
             {/*/.direct-chat */}
             {/* TO DO List */}
-            <div className="card">
+            <div className="card" style={{width:1050}}>
               <div className="card-header">
                 <h3 className="card-title">
                   <i className="ion ion-clipboard mr-1" />
-                  To Do List
+                  New Registrations
                 </h3>
                 <div className="card-tools">
                   <ul className="pagination pagination-sm">
@@ -441,95 +257,36 @@ export default class Dashboard extends Component {
           {/* /.Left col */}
           {/* right col (We are only adding the ID to make the widgets sortable)*/}
           <section className="col-lg-5 connectedSortable">
-            {/* Map card */}
-            <div className="card bg-gradient-primary">
-              <div className="card-header border-0">
-                <h3 className="card-title">
-                  <i className="fas fa-map-marker-alt mr-1" />
-                  Visitors
-                </h3>
-                {/* card tools */}
-                <div className="card-tools">
-                  <button type="button" className="btn btn-primary btn-sm daterange" data-toggle="tooltip" title="Date range">
-                    <i className="far fa-calendar-alt" />
-                  </button>
-                  <button type="button" className="btn btn-primary btn-sm" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                    <i className="fas fa-minus" />
-                  </button>
-                </div>
-                {/* /.card-tools */}
-              </div>
-              <div className="card-body">
-                <div id="world-map" style={{height: 250, width: '100%'}} />
-              </div>
+           {/* Map card */}
+            <div >
+              
+            
               {/* /.card-body*/}
               <div className="card-footer bg-transparent">
                 <div className="row">
                   <div className="col-4 text-center">
                     <div id="sparkline-1" />
-                    <div className="text-white">Visitors</div>
+                 
                   </div>
                   {/* ./col */}
                   <div className="col-4 text-center">
                     <div id="sparkline-2" />
-                    <div className="text-white">Online</div>
+                   
                   </div>
                   {/* ./col */}
                   <div className="col-4 text-center">
                     <div id="sparkline-3" />
-                    <div className="text-white">Sales</div>
+                  
                   </div>
                   {/* ./col */}
                 </div>
                 {/* /.row */}
               </div>
             </div>
-            {/* /.card */}
-            {/* solid sales graph */}
-            <div className="card bg-gradient-info">
-              <div className="card-header border-0">
-                <h3 className="card-title">
-                  <i className="fas fa-th mr-1" />
-                  Sales Graph
-                </h3>
-                <div className="card-tools">
-                  <button type="button" className="btn bg-info btn-sm" data-card-widget="collapse">
-                    <i className="fas fa-minus" />
-                  </button>
-                  <button type="button" className="btn bg-info btn-sm" data-card-widget="remove">
-                    <i className="fas fa-times" />
-                  </button>
-                </div>
-              </div>
-              <div className="card-body">
-                <canvas className="chart" id="line-chart" style={{minHeight: 250, height: 250, maxHeight: 250, maxWidth: '100%'}} />
-              </div>
-              {/* /.card-body */}
-              <div className="card-footer bg-transparent">
-                <div className="row">
-                  <div className="col-4 text-center">
-                    <input type="text" className="knob" data-readonly="true" defaultValue={20} data-width={60} data-height={60} data-fgcolor="#39CCCC" />
-                    <div className="text-white">Mail-Orders</div>
-                  </div>
-                  {/* ./col */}
-                  <div className="col-4 text-center">
-                    <input type="text" className="knob" data-readonly="true" defaultValue={50} data-width={60} data-height={60} data-fgcolor="#39CCCC" />
-                    <div className="text-white">Online</div>
-                  </div>
-                  {/* ./col */}
-                  <div className="col-4 text-center">
-                    <input type="text" className="knob" data-readonly="true" defaultValue={30} data-width={60} data-height={60} data-fgcolor="#39CCCC" />
-                    <div className="text-white">In-Store</div>
-                  </div>
-                  {/* ./col */}
-                </div>
-                {/* /.row */}
-              </div>
-              {/* /.card-footer */}
-            </div>
+
             {/* /.card */}
             {/* Calendar */}
-            <div className="card bg-gradient-success">
+            <div className="card bg-gradient-success" style={{height:'39%'}}>
               <div className="card-header border-0">
                 <h3 className="card-title">
                   <i className="far fa-calendar-alt" />
--- frontend/src/dashboard/Menu.js
+++ frontend/src/dashboard/Menu.js
@@ -8,7 +8,7 @@ export default class Menu extends Component {
     {/* Brand Logo */}
     <a href="index3.html" className="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
-      <span className="brand-text font-weight-light">AdminLTE 3</span>
+      <span className="brand-text font-weight-light">Admin Dashboard</span>
     </a>
     {/* Sidebar */}
     <div className="sidebar">
@@ -18,7 +18,7 @@ export default class Menu extends Component {
           <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
         </div>
         <div className="info">
-          <a href="#" className="d-block">Alexander Pierce</a>
+          <a href="#" className="d-block">John </a>
         </div>
       </div>
       {/* Sidebar Menu */}
